a9ad02e64dcefb2ae98500d468e34a0f9615d9f0
Ben McMann <bbmcmann@volosys.com>
2019-11-12 12:19:49 -0700

    Delete Comments

--- src/Magpie3.java
+++ src/Magpie3.java
@@ -1,30 +1,10 @@
-/**
- * A program to carry on conversations with a human user. This version:
- * <ul>
- * <li>
- * Uses advanced search for keywords</li>
- * </ul>
- * 
- * @author Laurie White
- * @version April 2012
- */
 public class Magpie3 {
-	/**
-	 * Get a default greeting
-	 * 
-	 * @return a greeting
-	 */
+	
 	public String getGreeting() {
 		return "Hello, let's talk.";
 	}
 
-	/**
-	 * Gives a response to a user statement
-	 * 
-	 * @param statement
-	 *            the user statement
-	 * @return a response based on the rules given
-	 */
+	
 	public String getResponse(String statement) {
 		String response = "";
 		if (statement.length() == 0) {
@@ -42,20 +22,7 @@ public class Magpie3 {
 		return response;
 	}
 
-	/**
-	 * Search for one word in phrase. The search is not case sensitive. This
-	 * method will check that the given goal is not a substring of a longer
-	 * string (so, for example, "I know" does not contain "no").
-	 * 
-	 * @param statement
-	 *            the string to search
-	 * @param goal
-	 *            the string to search for
-	 * @param startPos
-	 *            the character of the string to begin the search at
-	 * @return the index of the first occurrence of goal in statement or -1 if
-	 *         it's not found
-	 */
+	
 	private int findKeyword(String statement, String goal, int startPos) {
 		String phrase = statement.trim().toLowerCase();
 		goal = goal.toLowerCase();
@@ -64,11 +31,9 @@ public class Magpie3 {
 		// the line below
 		int psn = phrase.indexOf(goal, startPos);
 
-		// Refinement--make sure the goal isn't part of a
-		// word
+		
 		while (psn >= 0) {
-			// Find the string of length 1 before and after
-			// the word
+			
 			String before = " ", after = " ";
 			if (psn > 0) {
 				before = phrase.substring(psn - 1, psn);
@@ -78,19 +43,11 @@ public class Magpie3 {
 						psn + goal.length() + 1);
 			}
 
-			// If before and after aren't letters, we've
-			// found the word
-			if (((before.compareTo("a") < 0) || (before.compareTo("z") > 0)) // before
-																				// is
-																				// not
-																				// a
-																				// letter
+			if (((before.compareTo("a") < 0) || (before.compareTo("z") > 0)) 
 					&& ((after.compareTo("a") < 0) || (after.compareTo("z") > 0))) {
 				return psn;
 			}
 
-			// The last position didn't work, so let's find
-			// the next, if there is one.
 			psn = phrase.indexOf(goal, psn + 1);
 
 		}
@@ -98,28 +55,12 @@ public class Magpie3 {
 		return -1;
 	}
 
-	/**
-	 * Search for one word in phrase. The search is not case sensitive. This
-	 * method will check that the given goal is not a substring of a longer
-	 * string (so, for example, "I know" does not contain "no"). The search
-	 * begins at the beginning of the string.
-	 * 
-	 * @param statement
-	 *            the string to search
-	 * @param goal
-	 *            the string to search for
-	 * @return the index of the first occurrence of goal in statement or -1 if
-	 *         it's not found
-	 */
+	
 	private int findKeyword(String statement, String goal) {
 		return findKeyword(statement, goal, 0);
 	}
 
-	/**
-	 * Pick a default response to use if nothing else fits.
-	 * 
-	 * @return a non-committal string
-	 */
+	
 	private String getRandomResponse() {
 		final int NUMBER_OF_RESPONSES = 4;
 		double r = Math.random();
--- src/MagpieRunner3.java
+++ src/MagpieRunner3.java
@@ -1,16 +1,9 @@
 import java.util.Scanner;
 
-/**
- * A simple class to run the Magpie class.
- * 
- * @author Laurie White
- * @version April 2012
- */
+
 public class MagpieRunner3 {
 
-	/**
-	 * Create a Magpie, give it user input, and print its replies.
-	 */
+	
 	public static void main(String[] args) {
 		Magpie3 maggie = new Magpie3();
 
